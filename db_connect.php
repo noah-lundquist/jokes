1ba442f0023ca0a1ab6ccfef9f97e335db7ae2f4
noah-lundquist <97271885+noah-lundquist@users.noreply.github.com>
2023-08-10 23:19:33 -0700

    Update db_connect.php

--- db_connect.php
+++ db_connect.php
@@ -1,9 +1,9 @@
 <?php
 // variables
-$host = "localhost";
-$username = "root";
-$user_pass = "root";
-$database_in_use = "jokes";
+$host = "jokesappgcu-server.mysql.database.azure.com";
+$username = "npzqwyhvnw";
+$user_pass = "Tallorder1!";
+
 
 // create connection string
 $mysqli = new mysqli($host, $username, $user_pass, $database_in_use);
